8821919662af8fafeab680f6cc50aebb03feabf8
marla294@gmail.com <marla294@gmail.com>
2023-09-08 07:10:27 -0500

    WIP styling comic component

--- src/routes/comic.tsx
+++ src/routes/comic.tsx
@@ -1,5 +1,22 @@
+import comic from '../assets/09082023.jpeg';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: grid;
+  justify-content: center;
+  align-content: center;
+`;
+
+const StyledComic = styled.img`
+  width: 400px;
+  height: 400px;
+  object-fit: cover;
+`;
+
 const Comic = () => {
-  return <div>Comic</div>
+  return <Container>
+    <StyledComic src={comic} />
+  </Container>
 }
 
 export default Comic;
