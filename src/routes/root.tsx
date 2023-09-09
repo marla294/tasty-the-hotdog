ee3250cdafe53f0152fde52058561c3622181780
marla294@gmail.com <marla294@gmail.com>
2023-09-09 06:45:03 -0500

    Moving page contents down

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,10 +1,17 @@
 import Header from "../components/Header";
 import { Outlet } from "react-router-dom";
+import styled from 'styled-components';
+
+const Container = styled.div`
+  margin-top: 2rem;
+`;
 
 const Root = () => {
   return <>
     <Header />
-    <Outlet />
+    <Container>
+      <Outlet />
+    </Container>
   </>
 }
 
