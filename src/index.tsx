7789f8f6677776624abc73d8a163c2e4b34657cb
marla294@gmail.com <marla294@gmail.com>
2023-10-10 06:52:39 -0500

    Remove About component

--- src/index.tsx
+++ src/index.tsx
@@ -1,41 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Root from './routes/root';
-import ErrorPage from './routes/error-page';
-import About from './routes/about';
-import Comic from './routes/comic';
-import Home from './routes/home';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./routes/root";
+import ErrorPage from "./routes/error-page";
+import Comic from "./routes/comic";
+import Home from "./routes/home";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-    errorElement: <ErrorPage errorMsg='An error has occurred' />,
+    errorElement: <ErrorPage errorMsg="An error has occurred" />,
     children: [
       {
         path: "",
         element: <Home />,
       },
       {
-        path: "about",
-        element: <About />,
-      },
-      {
         path: "comic/:id",
         element: <Comic />,
-      }
-    ]
+      },
+    ],
   },
 ]);
 
-ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
